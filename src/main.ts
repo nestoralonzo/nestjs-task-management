17786055af50f8b8da8eebfa07fffa7703f88535
Nestor Alonzo <nestoralonzo@hotmail.com>
2024-12-04 11:40:56 -0500

    ValidatationPipe - validate CreateTaskDto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Whenever app encounters any of those validation decorators
+  // (e.g. @IsNotEmpy), it will execute validation pipes.
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
