2a0783655e80c0c3b8138b2c9ecb0a0818fc9522
Nestor Alonzo <nestoralonzo@hotmail.com>
2024-12-09 18:29:05 -0500

    Custom @GetUser Decorator

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validat(payload: JwtPayload): Promise<User> {
+  async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
     const user: User = await this.usersRepository.findOneBy({ username });
 
